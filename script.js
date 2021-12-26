21d8514aa69935d350b5f9771042269ca2d8f723
gavin <gavin.lim@alumni.ubc.ca>
2021-12-25 16:18:44 -0800

    Add add, mul, divide, and sub functionlity

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var sec_op = "";        // Second operand
 var calc_str = "";      // Intermediate string for calculation
 var sum_val = "";       // Final value for sum
 
-let operator_pressed = false;
-let digit_pressed = false;
-
 var top_screen = document.getElementById('top-screen');
-var main_screen = document.getElementById('main-screen');
+var result_screen = document.getElementById('result-screen');
+
+var op_pressed = false;
+const op_array = new Set(['+', '-', '÷', 'x']);
+
 
 function add(a, b) {return a - b;}
 function sub(a, b) {return a - b;}
@@ -16,85 +17,76 @@ function mul(a, b) {return a * b;}
 function div(a, b) {return a / b;}
 function neg(a) {return -1 * a;}
 
-function operate(op, a, b) {
-    if (op == "add") {return add(a, b);}
-    if (op == "sub") {return sub(a, b);}
-    if (op == "mul") {return mul(a, b);}
-    if (op == "div") {return div(a, b);}
+function operate(a, op, b) {
+    if (op == '+') {return add(a, b);}
+    if (op == '-') {return sub(a, b);}
+    if (op == 'x') {return mul(a, b);}
+    if (op == '÷') {return div(a, b);}
     if (op == "neg") {return neg(a);}
 }
 
 function calculate_sum() {
-    display_str.split(' ').forEach(element => {
-        console.log(element);
-    });
+    const arr = display_str.split(' ');    
+
+    const operand = arr.find(element => op_array.has(element));
+    // const index = arr.findIndex(element => element == operand);
+    const index = arr.indexOf(operand);
+    const a = arr[index-1];
+    const b = arr[index+1];
+
+    // console.log('a: ' + a + ' | ' + 'b: ' + b + ' | ' + 'op: ' + operand);
+    
+    const result = operate(a, operand, b);
+    result_screen.innerHTML = result;
+
+    // display_str.split(' ').forEach(element => {
+    //     console.log(element);
+    // });
 }
 
 function display_digit() {
     display_str = display_str.concat(this.innerHTML);
     top_screen.innerHTML = display_str;
-    
-    digit_pressed = true;
-    console.log("digit: " + digit_pressed);
+    op_pressed = false;
 
     // Check for display overflow and adjust font size
-    adjust_display();    
+    adjust_display(); 
+
 }
 
 // Operator functions
 function display_operator() {
     const selection = this.innerHTML;
-    
+
     // Clear and reset
     if (selection == "C") {
         display_str = "";
         top_screen.innerHTML = display_str;  
-        main_screen.innerHTML = display_str;
+        result_screen.innerHTML = display_str;
         first_op = display_str;
         sec_op = display_str;
         operator_pressed = false;
         digit_pressed = false;
         top_screen.style.fontSize = "70px";
-        return;
     
     // Delete (Consider white spaces for operators)
     } else if (selection == "del") {
         display_str = display_str.slice(-1) == ' ' ? display_str.slice(0, -3) : display_str.slice(0, -1);
         top_screen.innerHTML = display_str;  
-        return;
     
     // Sum
     } else if (selection == "=") {
-        console.log("Operator: " + operator_pressed + " | " + "Digit: " + digit_pressed);
-        if (!operator_pressed) {
-            main_screen.innerHTML = display_str;
-        } else {
-            if (!digit_pressed) {return}
-            calculate_sum();
-        }
-        operator_pressed = false;
-        return;
-    } 
+        if (op_pressed) {return;}
+        calculate_sum();
 
-    if (!digit_pressed) {return}
+    } else {
+        if (op_pressed) {return;}
+        display_str = display_str.concat(selection);      
+        op_pressed = true;
+    }
 
-    // Rest of op
-    if (!operator_pressed) {
-        
-        // If operator pressed for the first time, 
-        // save first operand
-        first_op = display_str;
-        display_str = display_str.concat(selection);
-        top_screen.innerHTML = display_str;
-
-        operator_pressed = true;
-        digit_pressed = false;
-
-        console.log("operator: " + operator_pressed);
-
-        // Check for display overflow and adjust font size
-        adjust_display();
-    } 
+    top_screen.innerHTML = display_str;
+    adjust_display();
 }
 
 // Adjust fontsize of main display to prevent overflow
